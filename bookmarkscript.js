29b94c36e7ee8754b2e2c3ae0163ecfa5e08d439
lawreka <lawr33ka@gmail.com>
2016-01-13 21:35:42 -0800

    try new js

--- bookmarkscript.js
+++ bookmarkscript.js
@@ -1,1 +1,9 @@
-console.log("Bookmark'd B]");
+var bod = document.getElementsByTagName("body");
+var list = document.createElement("ol");
+var list1 = document.createElement("li");
+var list2 = document.createElement("li");
+var list3 = document.createElement("li");
+bod.appendChild(list);
+list.appendChild(list1);
+list.appendChild(list2);
+list.appendChild(list3);
